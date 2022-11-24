39ea3679a57aa12344c8e4ae0c41b6b20a55bce7
ericjiji3 <69319444+ericjiji3@users.noreply.github.com>
2022-11-24 14:28:09 -0600

    eirhoe

--- src/index.js
+++ src/index.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Canvas, useLoader } from '@react-three/fiber';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import * as THREE from 'three';
+
+const Triangle = () => {
+  const loadTri = useLoader(GLTFLoader, '/triangle.glb');
+  let triangle;
+  loadTri.scene.traverse(child => {
+    if(child.name == 'Cylinder'){
+      triangle = child;
+      triangle.material = new THREE.MeshNormalMaterial();
+      triangle.position.y = -0.15;
+    }
+  })
+
+  for(let i = 0; i < 20; i++){
+    const t = triangle.scene.clone();
+    console.log(t);
+    return(
+      <Suspense fallback={null}>
+        <primitive object={t}/>
+      </Suspense>
+    )
+  }
+  
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Canvas camera={{fov: 75, position: [0, 0, 3]}} style={{height: '100vh', width: '100%', backgroundColor: '#000000'}}>
+    <Triangle/>
+  </Canvas>
 );
 
 // If you want to start measuring performance in your app, pass a function
