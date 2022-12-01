b81b13922218804ce16a36fda00dd065c308db3f
ericjiji3 <69319444+ericjiji3@users.noreply.github.com>
2022-12-01 13:31:58 -0600

    finish

--- src/index.js
+++ src/index.js
@@ -1,60 +1,60 @@
-import React, { useState, Suspense, useEffect } from 'react';
+import React, { useState, Suspense, useEffect, useRef, createRef } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Canvas, useLoader } from '@react-three/fiber';
+import { Canvas, useLoader, useFrame } from '@react-three/fiber';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import * as THREE from 'three';
 
 
 const Triangle = ({count}) => {
+  const animate = useRef([]);
+  
   const loadTri = useLoader(GLTFLoader, '/triangle.glb');
   const [clones, setClones] = useState();
-
+  console.log(animate.current)
   let triangle;
+
   loadTri.scene.traverse(child => {
     if(child.name == 'Cylinder'){
       triangle = child;
       triangle.material = new THREE.MeshNormalMaterial();
     }
   })
-
-  // const updatePosition = (tris) => {
-  //   foreach(tri in tris){
-
-  //   }
-  // }
-
+  
   useEffect(() => {
     const tempTri = [];
     for(let i=0; i<count; i++){
       let triType = {
-        scale: i
+        scale: i/2
       }
-      console.log(triType); 
+
       tempTri.push(triType);
     }
-    // updatePosition(tempTri);
-    console.log(tempTri);
+
     setClones(tempTri);
+    
   }, [count])
-  // const group = [];
-  // for(let i = 0; i < 20; i++){
-  //   const t = triangle.clone();
-  //   console.log(t);
-  //   group.push()
-  //   return(
-  //     <Suspense fallback={null}>
+
+  useFrame((state, delta) => (
+      animate.current.map((ani, i) => {
+        ani.rotation.x += 0.005 + i*0.0001
+        // if(i%2 == 0){
+        //   ani.rotation.y -= 0.005
+        // }else{
+        //   ani.rotation.y += 0.005
+        // }
         
-  //     </Suspense>
-  //   )
-  // }
+      })
+    )
+  )
+
   return(
-    <group>
+    <group >
         {clones?.map((clone, index) => {
           return(
-            <object3D key={index} scale={clone.scale} position={[0, 0, 0]}>
+            <object3D ref={el => animate.current[index] = el} key={index} scale={clone.scale} position={[0, 0, 0]}>
               <primitive object={triangle.clone()}/>
             </object3D>
           )
@@ -66,8 +66,8 @@ const Triangle = ({count}) => {
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Canvas camera={{fov: 75, position: [0, 2, 10]}} style={{height: '100vh', width: '100%', backgroundColor: '#000000'}}>
-    <Triangle count={5}/>
+  <Canvas camera={{fov: 75, position: [0, 2, 9]}} style={{height: '100vh', width: '100%', backgroundColor: '#000000'}}>
+    <Triangle count={15}/>
   </Canvas>
 );
 
